c4092fe45c355565836de090c460f54c01d9de53
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2019-01-06 14:46:14 +0400

    Добавил ConnectionString, Разрешение зависимостей.

--- WebStore.ServicesHosting/Startup.cs
+++ WebStore.ServicesHosting/Startup.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebStore.DAL.Context;
+using WebStore.Infrastructure.Implementations;
+using WebStore.Infrastructure.Implementations.Sql;
+using WebStore.Interfaces;
 
 namespace WebStore.ServicesHosting
 {
@@ -24,6 +29,14 @@ namespace WebStore.ServicesHosting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<WebStoreContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnetion")));
+
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+            services.AddTransient<IProductData, SqlProductData>();
+            services.AddTransient<IOrdersService, SqlOrdersService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
