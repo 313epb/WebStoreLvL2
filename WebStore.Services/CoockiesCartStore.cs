ed873b0a367c9c13e94e10ea1ebb565897c91bb6
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2019-01-09 14:49:52 +0400

    Разрешение зависимости для CoockierCartService. Добавление интерфейса IcartStore, класса CoockiesCartStore

---
+++ WebStore.Services/CoockiesCartStore.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using WebStore.DomainNew.Models.Cart;
+using WebStore.Interfaces;
+
+namespace WebStore.Services
+{
+    public class CoockiesCartStore:ICartStore
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _cartName;
+        public Cart Cart { get; set; }
+
+        public CoockiesCartStore(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}
