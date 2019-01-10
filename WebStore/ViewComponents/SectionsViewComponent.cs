cb096e5792d9de639311ab50911c0807c5607acd
Дерюгин Дмитрий Сергеевич <dsderugin@gmail.com>
2019-01-10 10:56:45 +0400

    Добавлена навигация в меню и в LeftSideBar

--- WebStore/ViewComponents/SectionsViewComponent.cs
+++ WebStore/ViewComponents/SectionsViewComponent.cs
@@ -11,29 +11,33 @@ namespace WebStore.ViewComponents
     public class SectionsViewComponent : ViewComponent
     {
         private readonly IProductData _productData;
-
         public SectionsViewComponent(IProductData productData)
         {
             _productData = productData;
         }
-
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string sectionId)
         {
-            var sections = GetSections();
-            return View(sections);
+            int.TryParse(sectionId, out var sectionIdInt);
+            var sections = GetSections(sectionIdInt, out var parentSectionId);
+            return View(new SectionCompleteViewModel()
+            {
+                Sections = sections,
+                CurrentSectionId = sectionIdInt,
+                CurrentParentSectionId = parentSectionId
+            });
         }
-
         /// <summary>
         /// Получает секции из базы и строит дерево
         /// </summary>
         /// <returns></returns>
-        private List<SectionViewModel> GetSections()
+        private List<SectionViewModel> GetSections(int? sectionId, out int?
+            parentSectionId)
         {
+            parentSectionId = null;
             var allSections = _productData.GetSections();
-
-            var parentCategories = allSections.Where(p => !p.ParentId.HasValue).ToArray();
+            var parentCategories = allSections.Where(p =>
+                !p.ParentId.HasValue).ToArray();
             var parentSections = new List<SectionViewModel>();
-
             foreach (var parentCategory in parentCategories)
             {
                 parentSections.Add(new SectionViewModel()
@@ -44,12 +48,14 @@ namespace WebStore.ViewComponents
                     ParentSection = null
                 });
             }
-
             foreach (var sectionViewModel in parentSections)
             {
-                var childCategories = allSections.Where(c => c.ParentId.Equals(sectionViewModel.Id));
+                var childCategories = allSections.Where(c =>
+                    c.ParentId.Equals(sectionViewModel.Id));
                 foreach (var childCategory in childCategories)
                 {
+                    if (childCategory.Id == sectionId)
+                    parentSectionId = sectionViewModel.Id;
                     sectionViewModel.ChildSections.Add(new SectionViewModel()
                     {
                         Id = childCategory.Id,
@@ -58,12 +64,12 @@ namespace WebStore.ViewComponents
                         ParentSection = sectionViewModel
                     });
                 }
-                sectionViewModel.ChildSections = sectionViewModel.ChildSections.OrderBy(c => c.Order).ToList();
+                sectionViewModel.ChildSections =
+                    sectionViewModel.ChildSections.OrderBy(c => c.Order).ToList();
             }
-
             parentSections = parentSections.OrderBy(c => c.Order).ToList();
-
             return parentSections;
         }
     }
 }
+
